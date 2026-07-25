0fad13c8ae732e5a516ab9f0cbf859ea7ce477d1
Praveen <praveenkumar955895@gmail.com>
2026-07-25 20:41:42 +0530

    Create a MongoDb using Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import {collectionName, connection} from './dbconfig.js';
 const app =  express();
-app.get('/', (req, resp) => {
-    resp.send({
-        message:'welcome to nodejs project',
-        status:true
-    })
+app.use(express.json());
+
+app.post('/add-list',async (req,resp) => {
+    const db = await connection();
+    const collection = await db.collection(collectionName);
+    const result = await collection.insertOne(req.body);
+    if (result) {
+        resp.send({
+            message:'New List has been added succesfully.',
+            status:true,
+            result
+        })
+    } else {
+        resp.send({
+            message:'List has not been added.',
+            status:false
+        })
+    }
 })
 app.listen(3200);
